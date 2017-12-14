d171ec869c08452124c8a6176876b46fda159fd0
uenhoeyz <uenhoeyz@gmail.com>
2017-12-14 14:36:47 +0800

    Seperate free and paid versions

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,57 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.udacity.gradle.builditbigger.JokeMakerAsyncTask;
+import com.udacity.gradle.builditbigger.R;
+
+public class MainActivityFragment extends Fragment implements View.OnClickListener {
+
+    private JokeMakerAsyncTask task;
+
+    public MainActivityFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        AdView mAdView = root.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+        Button jokeButton = root.findViewById(R.id.btn_joke);
+        jokeButton.setOnClickListener(this);
+
+        return root;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (task != null) {
+            task.cancel(false);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_joke:
+                task = new JokeMakerAsyncTask();
+                task.execute((getContext()));
+                break;
+            default:
+                break;
+        }
+    }
+}
--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,35 +1,23 @@
 package com.udacity.gradle.builditbigger;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
-
-/**
- * A placeholder fragment containing a simple view.
- */
 public class MainActivityFragment extends Fragment implements View.OnClickListener {
 
     private JokeMakerAsyncTask task;
 
-    public MainActivityFragment() {}
+    public MainActivityFragment() {
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-
-        AdView mAdView = root.findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
 
         Button jokeButton = root.findViewById(R.id.btn_joke);
         jokeButton.setOnClickListener(this);
@@ -50,7 +38,7 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
         switch (view.getId()) {
             case R.id.btn_joke:
                 task = new JokeMakerAsyncTask();
-                task.execute((getContext()));
+                task.execute(getActivity());
                 break;
             default:
                 break;
